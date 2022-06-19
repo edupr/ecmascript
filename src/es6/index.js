7f6527daa3f4178bf8d0be51bc88a13f6b911d61
Edu <particularspam@gmail.com>
2022-06-18 23:23:10 -0500

    LET, CONST, Multilínea, Spread,  Desestructuración

--- src/es6/index.js
+++ src/es6/index.js
@@ -20,3 +20,48 @@ console.log(epicPhrase);
 
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+let lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit.\n"
++ "Quisquam, quos, quisquam. Quisquam, quos, quisquam.";
+
+// es6
+let lorem2 = `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+Quisquam, quos, quisquam. Quisquam, quos, quisquam.`;
+console.log(lorem);
+console.log(lorem2);
+
+// Destructuring
+let person = {
+    name: 'Oscar',
+    age: 30,
+    country: 'MX'
+};
+
+console.log(person.name, person.age, person.country);
+
+let { name, age, country } = person;
+console.log(name, age, country);
+
+// Spread Operator
+let team1 = ['Oscar', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Camila', 'Andrea'];
+
+let education = ['David', ...team1, ...team2]; // spread operator
+console.log(team1, team2);
+console.log(education);
+
+// let
+{
+    var globalVar = 'Global Var';
+}
+
+{
+    let globalLet = 'Global Let';
+}
+
+console.log(globalVar);
+console.log(globalLet);
+
+const a = 'b';
+a = 'c';
+console.log(a);
