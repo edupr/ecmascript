117a54b836705e7a493366fe899a710eee7beb16
Edu <particularspam@gmail.com>
2022-06-19 19:08:35 -0500

    Añadido ECMAScript 7: includes y potencias

---
+++ es7/index.js
@@ -0,0 +1,15 @@
+// es7
+// includes es7/array.js
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+if (numbers.includes(11)) {
+    console.log('Yes');
+    } else {
+    console.log('No');
+};
+
+// exponentiation operator
+let base = 4;
+let exponent = 4;
+let result = base ** exponent;
+console.log(result);
