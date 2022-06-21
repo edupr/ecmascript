ce138b3d83a807d91ee997633d0a373d59637cf1
Edu <particularspam@gmail.com>
2022-06-20 21:34:10 -0500

    Async/Await

--- es8/index.js
+++ es8/index.js
@@ -34,3 +34,34 @@ const obj = {
     country: 'MX',
     job: 'Developer',   
 };
+
+
+// es8
+// Async/Await
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+            ? setTimeout(() => {
+                resolve('Hello World');
+            }, 3000)    
+            : reject(new Error('Test Error'));
+    });
+}
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+anotherFunction();
