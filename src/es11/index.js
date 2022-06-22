7e7256b1c41346651015a7a358bd902291bc421b
Edu <particularspam@gmail.com>
2022-06-22 15:56:03 -0500

    ES11

--- src/es11/index.js
+++ src/es11/index.js
@@ -6,3 +6,35 @@ button.addEventListener("click", async function () {
     module.hello();
 });
 
+// BigInt
+const aBigNumber = 9008768765836854678n;
+const anotherBigNumber = BigInt(9008768765836854678);
+
+console.log(aBigNumber);
+console.log(anotherBigNumber);
+
+// Promise
+const promise1 = new Promise((resolve, reject) => reject("reject"));
+const promise2 = new Promise((resolve, reject) => resolve("resolve"));
+const promise3 = new Promise((resolve, reject) => resolve("resolve 1"));
+
+Promise.allSettled(promise1, promise2, promise3)
+    .then(response => console.log(response));
+
+// GlobalThis
+// console.log(window);
+console.log(globalThis);
+
+// ?? (null operator)
+const fooo = 'asd' ?? 'default string';
+console.log(fooo);
+
+// Optional Chaining
+const user = {};
+console.log(user?.profile?.email);
+
+if (user?.profile?.email) {
+    console.log('email');
+} else {
+    console.log('no email');
+}
